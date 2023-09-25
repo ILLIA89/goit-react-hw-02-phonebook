0ee035f50c043733ec6a5d47dde40f1f68abee8e
ILLIA89 <anolij1@gmail.com>
2023-09-25 21:00:40 +0200

    app,  filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
+import { Component } from 'react';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+import style from './App.module.css';
+import { render } from '@testing-library/react';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+}
+
+handleAddNewContact = newContact => {
+  this.setState(prevState => ({
+    contacts: [...prevState.contacts, newContact],
+  }));
+};
+
+changeFilter = event => {
+  const { contacts, filter } = this.state;
+  const normalizeFilter = filter.toLowerCase();
+
+  return contacts.filter(({ name }) => name.toLowerCase().includes(normalizeFilter));
+};
+
+getVisibleContacts = () => {
+  const { contacts, filter } = this.state;
+  const normalizeFilter = filter.toLowerCase();
+
+  return contacts.filter(({ name }) => name.toLowerCase().includes(normalizeFilter));
+};
+
+handleDeleteContact = contactId => {
+  this.setState(prevState => ({
+    contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+  }));
+}
+
+render(){
+  const { contacts, filter } = this.state;
+  const visibleContacts = this.getVisibleContacts();
+  const contactsName = contacts.map(contact => contact.name);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1 className={style.title}>Phonebook</h1>
+      <ContactForm
+        onSubmit={this.handleAddNewContact}
+        contactsName={contactsName}
+      />
+
+      <h2 className={style.title}>Contacts</h2>
+      <div className={style.contact_list_container}>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          visibleContacts={visibleContacts}
+          onDeleteContact={this.handleDeleteContact}
+        />
+      </div>
     </div>
   );
 };
+
+export default App;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,22 @@
+import style from './Filter.module.css';
+
+const Filter = ({ value, onChange }) => (
+  <div className={style.searchContainer}>
+    {' '}
+    {/* Додаємо обгортку для центрування */}
+    <label className={style.search}>
+      <input
+        type="text"
+        name="filter"
+        placeholder=" "
+        className={style.inputName}
+        title="Enter search name"
+        onChange={onChange}
+        value={value}
+      />
+      <button type="reset"></button>
+    </label>
+  </div>
+);
+
+export default Filter;
